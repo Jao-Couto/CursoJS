6817dd0da5af4c52718912c07a2cea1b4cd0ff49
Rafael Correia <rafael.n.correia@unesp.br>
2023-01-14 17:48:26 -0300

    pós-curso

--- ListPokemons/ConsumirApi.js
+++ ListPokemons/ConsumirApi.js
@@ -13,27 +13,40 @@ async function loadPokemons() {
         let pokemon = await (await fetch(url)).json()
         pokemon.results.map(async item =>  {
             let poke = await (await fetch(item.url)).json()
+            let name = poke.name[0].toUpperCase() + poke.name.slice(1)
             let section = document.createElement('section');
             section.style = "margin: 10px;"
 
-            section.innerHTML = `<h1 style="font-size: 30px; margin: 0;">${poke.name}</h1>
-                    <h2 style="font-size: 18px; margin: 10px;">Habilidades</h2>
-                    <ul> `;
-            poke.abilities.map(item => {
-                section.innerHTML += `<li>${item.ability.name}</li>`
-            })
-
-            section.innerHTML += `</ul>
-                    <h2 style="font-size: 18px; margin: 10px;">Tipo</h2>
-                    <ul> `;
-            poke.types.map(item => {
-                section.innerHTML += `<li>${item.type.name}</li>`
-            })
-            section.innerHTML += '</ul>';
-
-            listPokemons.appendChild(section)
-            ini += 20
+            if(name.startsWith("C")){
+                section.innerHTML = `
+                <h1 style="font-size: 30px; margin: 0;">
+                    ${name}
+                </h1>
+                <h2 style="font-size: 18px; margin: 10px;">
+                    Habilidades
+                </h2>`
+                
+                section.innerHTML += "<ul>"
+                
+                poke.abilities.map(item => {
+                    section.innerHTML += `<li>${item.ability.name}</li>`
+                })
+    
+                section.innerHTML += `
+                </ul>
+                
+                <h2 style="font-size: 18px; margin: 10px;">Tipo</h2>
+                
+                <ul>`;
+                poke.types.map(item => {
+                    section.innerHTML += `<li>${item.type.name}</li>`
+                })
+                section.innerHTML += '</ul>';
+    
+                listPokemons.appendChild(section)
+            }
         })
+        ini += 20
     }catch(err){
         let mostrar = document.createElement('h1');
         mostrar.style = "margin: 10px; color:red;"
